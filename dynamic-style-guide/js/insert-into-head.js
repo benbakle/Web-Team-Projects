3caca8a85b60ba1191b32dd020ea64548c4a9f27
Benjamin Bakle <bbakle@gmail.com>
2017-03-23 23:47:16 -0400

    Added insert-into-head to project

---
+++ dynamic-style-guide/js/insert-into-head.js
@@ -0,0 +1,8 @@
+﻿var insertIntoHead = function (text) {
+    $("head").append(text);
+}
+
+$(".submit-head-text").on("click", function () {
+    var text = $(".head-text").val().trim();
+    insertIntoHead(text);
+})
